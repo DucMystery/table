bf62803aca4be8dc418531c7227ed028b90df8dc
DucMystery <58973644+DucMystery@users.noreply.github.com>
2020-07-28 08:05:20 +0700

    Them bai tap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MenuComponent} from './menu/menu.component';
+import {UserListComponent} from './users/user-list/user-list.component';
+import { AwesomeCalculatorComponent } from './awesome-calculator/awesome-calculator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    UserListComponent,
+    AwesomeCalculatorComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/awesome-calculator/awesome-calculator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-awesome-calculator',
+  templateUrl: './awesome-calculator.component.html',
+  styleUrls: ['./awesome-calculator.component.css']
+})
+export class AwesomeCalculatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
